11555fb96f833e790f6ba4d7f642b01009f4f568
A12989x <alexis12989x@gmail.com>
2020-05-08 16:17:21 -0500

    logo component and image added

---
+++ src/Components/Logo.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ds_circle_logo from '../img/ds_circle_logo.png';
+
+const Logo = () => {
+    return (
+        <img className="logo" alt="daily smarty logo" src={ds_circle_logo} />
+    );
+};
+
+export default Logo;
