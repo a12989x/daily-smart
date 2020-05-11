52bef4f75abedabadd5c54b89292c8ecb3056ac1
A12989x <alexis12989x@gmail.com>
2020-05-11 11:36:58 -0500

    used axios to perform get request to fetch recent posts and setup the required user

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,11 +1,20 @@
+import axios from 'axios';
+
+import { SET_RECENT_POSTS } from './types';
+
 export const fetchRecentPosts = () => {
     return function (dispatch) {
-        console.log('hello');
+        axios
+            .get('https://api.dailysmarty.com/posts')
+            .then((response) => {
+                console.log(response.data.posts);
+                dispatch({
+                    type: SET_RECENT_POSTS,
+                    payload: response.data.posts,
+                });
+            })
+            .catch((error) => {
+                console.error('fetchRecentPosts', error);
+            });
     };
 };
-
-// export function fetchRecentPosts() {
-//     return function (dispatch) {
-//         console.log('hello');
-//     };
-// }
