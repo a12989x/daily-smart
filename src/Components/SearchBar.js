80cefee0f3d9287a27265152d38f3cb15946e7ca
A12989x <alexis12989x@gmail.com>
2020-05-14 15:55:29 -0500

    implemented fontawesome library and icon

--- src/Components/SearchBar.js
+++ src/Components/SearchBar.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import { Field, reduxForm } from 'redux-form';
 import { withRouter } from 'react-router-dom';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faSearch } from '@fortawesome/free-solid-svg-icons';
 
 class SearchBar extends Component {
     constructor(props) {
@@ -27,7 +29,7 @@ class SearchBar extends Component {
                 onSubmit={handleSubmit(this.handleFormSubmit)}
             >
                 <label className="searchbar__label">
-                    Search DailySmarty
+                    <FontAwesomeIcon icon={faSearch} /> Search DailySmarty
                     <Field name="query" component={this.renderInput} />
                 </label>
             </form>
